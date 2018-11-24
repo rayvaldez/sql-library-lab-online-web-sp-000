469efd295f887aa3ccd223debb94a70c651a78d9
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-24 12:40:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
-  JOIN subgenres ON subgenre_id = authors.id;"
+  JOIN subgenres ON subgenre.id = authors.id;"
 end
 
 def select_series_title_with_most_human_characters
