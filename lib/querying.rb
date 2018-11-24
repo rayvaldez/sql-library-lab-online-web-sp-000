8301efce5dabe372420e657b63d1f0c7256a7b6b
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-24 12:51:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,5 +28,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(books.title) FROM characters;"
+  "SELECT characters.name, COUNT(books.title) FROM characters
+  JOIN series ON series.id = characters.series_id;"
 end
