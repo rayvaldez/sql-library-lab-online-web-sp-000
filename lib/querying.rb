c746630d0d8b9e96db1f92dcd6847a76f04f0588
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-24 12:46:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
-  GROUP BY characters.species;"
+  GROUP BY characters.species
+  WHERE characters.species = "human";"
 end
 
 def select_character_names_and_number_of_books_they_are_in
