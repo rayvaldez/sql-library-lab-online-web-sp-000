37e28d80d74f7b03ef905f8eae9c92493487aaa1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-24 13:57:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,6 +32,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.id) FROM character_books
   JOIN characters ON character_books.character_id = characters.id
-  GROUP BY characters.name
-  ORDER BY character_books.id DESC;"
+  GROUP BY characters.name"
 end
